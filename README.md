78d0c8fbd74808a59a3276998bb74393dfbe4adf
Iñigo Iparragirre <inigoiparraguirre@gmail.com>
2017-12-29 10:53:10 +0100

    First commit and setup files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-example-vs
+## VS Code C++ Code Demo
+
+1. Install VS Code Cpp tools
+2. Add c_cpp_properties.json file
+3. Create /build folder
+4. Create CMakeLists.txt
+5. Add tasks.json and create tasks form CMake files and compilation (MinGW setup)
---
+++ build/CMakeFiles/example-vs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example-vs.dir/main.cpp.obj"
+  "example-vs.pdb"
+  "example-vs.exe"
+  "example-vs.exe.manifest"
+  "libexample-vs.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example-vs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
